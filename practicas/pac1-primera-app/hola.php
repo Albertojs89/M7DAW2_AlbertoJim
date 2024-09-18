8e1912773ac8e07736c46c3334727943e8e4ed9a
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-09-18 14:04:56 +0000

    second_commit

---
+++ practicas/pac1-primera-app/hola.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="/practicas/pac1-primera-app/logofp.jpg" alt="">
+        <h1>Módulo 1- Práctica 1. Mi primera aplicación en PHP</h1>
+    </header>
+    <main>
+        <div>
+            <img src="/practicas/pac1-primera-app/avatar.jpg" alt="">
+        </div>
+        <div>
+            <p>
+                Function sayHello($name) es un método creado para saludar al nombre que el usuario ponga en la variable $name mediante la linea en la funcion de "echo "hello $name!;
+                
+                En el siguiente bloque 
+            </p>
+            
+        </div>
+    </main>
+
+    <footer>
+        <p>Alberto Jiménez Sánchez</p>
+        <data value="2024-09-16"></data>
+    </footer>
+    
+
+</body>
+</html>
