d07a7b568d734fe33c4b47d19b6a576cbf12a0f1
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-09-19 15:42:54 +0000

    commit4

---
+++ practicas/pac1-primera-app/practica2/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Indice</title>
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+    <header>
+        <h1>PRÁCTICA 1- INDICE</h1>
+    </header>
+
+    <main>
+        <?php
+            echo "<br>";
+            
+            echo "<h1>Ejercicios:</h1>";
+            echo "<br>";
+
+        ?>
+        <div>
+            <ul>
+                <li><a target="_blank" href="ejercicio1">Ejercicio 1</a></li>
+                <li><a target="_blank" href="ejercicio2">Ejercicio 2</a></li>
+                <li><a target="_blank" href="ejercicio3">Ejercicio 3</a></li>
+            </ul>
+        </div>
+            
+        
+    </main>
+</body>
+</html>
+
+
+
