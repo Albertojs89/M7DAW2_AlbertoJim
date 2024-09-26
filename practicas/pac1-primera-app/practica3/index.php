0db76bffe36d384ee1612ca4dbff3e500102472a
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-09-26 16:30:01 +0000

    commit12

--- practicas/pac1-primera-app/practica3/index.php
+++ practicas/pac1-primera-app/practica3/index.php
@@ -147,7 +147,7 @@
               </svg>
 
               <div class="card-body">
-                <p class="card-text">{$idolo[descripcion]}</p>
+                <p class="card-text">$idolo[descripcion]}</p>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
                     <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
