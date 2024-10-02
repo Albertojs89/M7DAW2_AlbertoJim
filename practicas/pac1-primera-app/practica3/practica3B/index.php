58c0fac622dd98790364a26f95c0ded9cb4ae190
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2024-10-02 15:44:27 +0000

    sacarImagen

--- practicas/pac1-primera-app/practica3/practica3B/index.php
+++ practicas/pac1-primera-app/practica3/practica3B/index.php
@@ -5,26 +5,23 @@
     <title>Frutas favoritas</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
+<style>
+    .casillaVerde{
+        background-color: #20c997;
+        font-weight: 700;
+    }
+</style>
 <body>
     <div class="container mt-5">
-        <h1 class="text-center">Selecciona tu fruta favorita</h1>
-
-        <table class="table table-bordered mt-4">
-            <thead class="thead-dark">
-                <tr>
-                    <th>Fruta</th>
-                    <th>Estado</th>
-                    <th>Acción</th>
-                </tr>
-            </thead>
+        
             <!-- array de frutas con su nombre -->
             <?php
                 $frutas=[
-                    ["nombre"=>"Manzana","estado"=>"0"],
-                    ["nombre"=>"Plátano"],
-                    ["nombre"=>"Naranja"],
-                    ["nombre"=>"Fresa"],
-                    ["nombre"=>"Kiwi"],
+                    ["nombre"=>"Manzana","imagen"=>"https://w7.pngwing.com/pngs/560/910/png-transparent-an-apple-green-apple-fruit-green.png"],
+                    ["nombre"=>"Plátano","imagen"=>"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.es%2Fvectores%2Fplatano-png&psig=AOvVaw2ppX8W9bF6-vbjoxieIJB0&ust=1727969288095000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiaj7GB8IgDFQAAAAAdAAAAABAE"],
+                    ["nombre"=>"Naranja","imagen"=>"https://static.vecteezy.com/system/resources/thumbnails/040/750/078/small_2x/ai-generated-orange-orange-isolated-refreshing-citrus-fruit-orange-slices-orange-top-view-orange-flat-lay-png.png"],
+                    ["nombre"=>"Fresa","imagen"=>"https://png.pngtree.com/png-clipart/20230410/original/pngtree-strawberry-realistic-fruit-png-image_9042320.png"],
+                    ["nombre"=>"Kiwi","imagen"=>"https://e7.pngegg.com/pngimages/166/911/png-clipart-kiwi-kiwi.png"],
                 ];
             ?>
 
@@ -39,67 +36,67 @@
                     <th>Acción</th>
                 </tr>
             </thead>
-            <!-- php bucle -->
+            <tbody>
+            <!-- php condicion -->
             <?php
-            
-                if(isset($_GET['nombre'])&&isset($_GET['estado'])){
-                    $nombre=$_GET['nombre'];
-                    $estado=$_GET['estado'];
-                    if($estado==1){
-                        echo '
-                            <tr class="table-danger">
-                            <td>'.$nombre.'</td>
-                            <td>Seleccionada</td>
-                            <td><a class="btn btn-primary" href="index.php?nombre=Manzana&estado=1">Seleccionar</a></td>
-                </tr>
+            //Con el isset getter cojemos el 'nombre'enviado desde el boton y lo guardamos en la variable nombre.
+                if(isset($_GET['nombre'])){
+                    $nombre=$_GET['nombre']; 
+                    
+                    
+                    //EXPLICACION FOREACH-------------------------------------------------------------------------------
+                    //Ahora con un foreach repasamos todos los elementos de la array frutas, comparando 
+                    //cada fruta y su nombre con el getter en la variable nombre
+                    //Si coincide: imprimimos el html de la tabla con la posicion de la array fruta[nombre]
+
+                    foreach($frutas as $fruta){
+                        if($fruta["nombre"]==$nombre){
+                            
+                            echo '
+                                <tr class="casillaVerde">
+                                <td>'.$fruta['nombre'].'</td>
+                                <td>✔️Seleccionada</td>
+                                <td><a class="btn btn-primary" href="?nombre='.$fruta['nombre'].'">Seleccionar</a></td>
+                                </tr>
+                                
+                            ';
+                            echo '
+                            <div class="card mt-4 w-25 shadow-lg">
+                                <img src="" class="card-img-top img-fluid" alt="'.$fruta['nombre'].'">
+                                <div class="card-body bg-warning">
+                                    <h5 class="card-title">'.$fruta['nombre'].'</h5>
+                                    <p class="card-text">¡Esta es tu fruta favorita!</p>
+                                </div>
+                            </div>
                         ';
-                    }else{
-                        echo '
-                            <tr class="table-danger">
-                            <td>'.$nombre.'</td>
-                            <td>✖ No seleccionada</td>
-                            <td><a class="btn btn-primary" href="index.php?nombre=Manzana&estado=1">Seleccionar</a></td>
-                </tr>
-                        ';
+                            
+                        }else{
+                            echo '
+                                <tr class="table-danger">
+                                <td>'.$fruta['nombre'].'</td>
+                                <td>✖Seleccionada</td>
+                                <td><a class="btn btn-primary" href="?nombre='.$fruta['nombre'].'">Seleccionar</a></td>
+                                </tr>
+                            ';
+                            
+                        }
+                        
                     }
+                    
                     
                 }
             
             ?>
 
-<!-- 
-            <tbody>
-                <tr class="table-danger">
-                    <td>Manzana</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="index.php?nombre=Manzana&estado=1">Seleccionar</a></td>
-                </tr>
-                <tr class="table-danger">
-                    <td>Plátano</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="">Seleccionar</a></td>
-                </tr>
-                <tr class="table-danger">
-                    <td>Naranja</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="">Seleccionar</a></td>
-                </tr>
-                <tr class="table-danger">
-                    <td>Fresa</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="">Seleccionar</a></td>
-                </tr>
-                <tr class="table-danger">
-                    <td>Kiwi</td>
-                    <td>✖ No seleccionada</td>
-                    <td><a class="btn btn-primary" href="">Seleccionar</a></td>
-                </tr>
-            </tbody>
-        </table> -->
 
+
+        </table>
+                <?php
+                   
+                ?>
         <!-- Mostrar tarjeta de la fruta seleccionada (actualmente estatica, siempre hay una manzana) -->
         <div class="card mt-4 w-25 shadow-lg">
-            <img src="https://static.vecteezy.com/system/resources/previews/023/858/853/non_2x/ai-genrative-green-apple-free-png.png" class="card-img-top img-fluid" alt="Manzana">
+            <img src="" class="card-img-top img-fluid" alt="Manzana">
             <div class="card-body bg-warning">
                 <h5 class="card-title">Manzana</h5>
                 <p class="card-text">¡Esta es tu fruta favorita!</p>
@@ -112,3 +109,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
+
+
