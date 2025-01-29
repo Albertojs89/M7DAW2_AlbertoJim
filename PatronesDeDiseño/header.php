60efd22d722fd2088cb08cc763f991155d2ce109
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2025-01-29 16:27:50 +0000

    terminar vistas web

---
+++ PatronesDeDiseño/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<link rel="stylesheet" href="estilos.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Karla+Tamil+Inclined:wght@400;700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+  rel="stylesheet"
+/>
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+<link
+  href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Karla+Tamil+Inclined:wght@400;700&family=Noto+Sans+Elbasan&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+  rel="stylesheet"
+/>
+<script src="https://kit.fontawesome.com/c5ee713d6d.js" crossorigin="anonymous"></script>
+
+
+<body>
+    <header class="fixed-top">
+        <nav class="navbar">
+            <ul class="nav py-3">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php"><i class="fa-solid fa-house"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="estructurals.php">Estructurales</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="creacion.php">De Creación</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="comportament.php">De Comportamiento</a>
+                </li>
+                
+            </ul>
+            <div class="mx-5" style="font-size: 35px;">
+              <a href="index.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
---
+++ PatronesDeDiseño/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Patrons de Disseny</title>
+  </head>
+  <link rel="stylesheet" href="estilos.css" />
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+    crossorigin="anonymous"
+  />
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Karla+Tamil+Inclined:wght@400;700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet"
+  />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Karla+Tamil+Inclined:wght@400;700&family=Noto+Sans+Elbasan&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+    rel="stylesheet"
+  />
+  <script
+    src="https://kit.fontawesome.com/c5ee713d6d.js"
+    crossorigin="anonymous"
+  ></script>
+
+  <body>
+    <main>
+    
+      
+          
+      <article
+        class="container-fluid d-flex justify-content-center align-items-center"
+        style="height: 100vh"
+      >
+      <h1 class="titulo">Patrons de Disseny</h1>
+        
+          <article>
+          <div class="container text-center d-flex justify-content-center">
+            <div class="row">
+              <div class="col-auto shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <div class="img-container img-hover-container position-relative">
+                  <a href="estructurals.php">
+                    <img src="https://img.freepik.com/vector-gratis/telon-fondo-abstracto-estilo-poli-presentacion-moderna_1017-53594.jpg" alt="" class="img-fluid" />
+                    <i class="fa-solid fa-sitemap hover-text position-absolute top-50 start-50 translate-middle mb-5" style="font-size: 60px;"></i>
+                    <span class="hover-text position-absolute top-50 start-50 translate-middle mt-5">Estructurales</span>
+                  </a>
+                </div>
+              </div>
+               <div class="col-auto shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <div class="img-container img-hover-container position-relative">
+                  <a href="creacion.php">
+                    <img src="https://img.freepik.com/vector-gratis/telon-fondo-abstracto-estilo-poli-presentacion-moderna_1017-53594.jpg" alt="" class="img-fluid" />
+                    <i class="fa-solid fa-helmet-safety hover-text position-absolute top-50 start-50 translate-middle mb-5" style="font-size: 60px;"></i>
+                    <span class="hover-text position-absolute top-50 start-50 translate-middle mt-5">De Creación</span>
+                  </a>
+                </div>
+              </div>
+               <div class="col-auto shadow-lg p-3 mb-5 bg-body-tertiary rounded">
+                <div class="img-container img-hover-container position-relative">
+                  <a href="comportament.php">
+                    <img src="https://img.freepik.com/vector-gratis/telon-fondo-abstracto-estilo-poli-presentacion-moderna_1017-53594.jpg" alt="" class="img-fluid" />
+                    <i class="fa-solid fa-binoculars hover-text position-absolute top-50 start-50 translate-middle mb-5" style="font-size: 60px;"></i>
+                    <span class="hover-text position-absolute top-50 start-50 translate-middle mt-5">Comportamiento</span>
+                  </a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </article>
+      </article>
+
+
+
+
+      <!-- buscador de items de la array -->
+
+      <article>
+       
+      </article>
+
+
+
+
+
+      
+    </main>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
