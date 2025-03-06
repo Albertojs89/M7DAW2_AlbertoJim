04354b82b20f75df936a2195e1521c2bbdfdcd91
Albertojs89 <156692647+Albertojs89@users.noreply.github.com>
2025-03-06 17:32:55 +0000

    upp header

--- CRUD/plantilla/theme/index.php
+++ CRUD/plantilla/theme/index.php
@@ -60,7 +60,7 @@ $usuarios=$result->fetch_all(MYSQLI_ASSOC); //hacemos que result coja todo y lo 
   </head>
 
   <body>
-    <header class="bg-light py-3">
+<header class="bg-light py-3">
       <div class="container">
         <div class="row align-items-center">
           <div class="col-md-6">
@@ -68,33 +68,25 @@ $usuarios=$result->fetch_all(MYSQLI_ASSOC); //hacemos que result coja todo y lo 
           </div>
           <div class="col-md-6 text-md-right">
             <nav>
-              <?php
-              
-                if (isset($_SESSION['user_id'])): ?>
-                  <img src="<?= $_SESSION['avatar'] ?>" alt="">
-                  <p><?= $_SESSION['username'] ?></p>
-                  <a href="logout.php" class="btn btn-primary"><img src="" alt=""></a>
-                  <?php if ($_SESSION['role'] == 'admin'): ?>
-                    <a href="admin.php" class="btn btn-primary"><img src="./images/rueda.png" alt=""></a>
-                  <?php endif; ?>
+              <?php if (isset($_SESSION['user_id'])): ?>
+                <img src="<?= $_SESSION['avatar'] ?>" alt="" class="avatar">
+                <p class="d-inline"><?= $_SESSION['username'] ?></p>
+                <br>
+                <a href="logout.php" class="btn btn-primary mt-2">Cerrar sesión<img src="" alt=""></a>
+                <?php if ($_SESSION['role'] == 'admin'): ?>
+                  <a href="admin.php" class="btn btn-primary"><img src="./images/rueda.png" alt=""></a>
+                <?php else: ?>
+                  <img src="./images/user.png" alt="" class="user-icon">
                 <?php endif; ?>
-            
-              <ul class="list-inline mb-0">
-                <li class="list-inline-item"><a href="#" class="text-dark">Home</a></li>
-                <li class="list-inline-item"><a href="#" class="text-dark">About</a></li>
-                <li class="list-inline-item"><a href="#" class="text-dark">Contact</a></li>
-              </ul>
-            </nav>
+              <?php endif; ?>
+              
           </div>
         </div>
       </div>
     </header>
     <!-- crear tabla con informacion de la bd usuarios -->
     <div>
-      <?php foreach($usuarios as $usuario){
-        
-      }
-      ?>
+      
 
     </div>
     <header class="navigation fixed-top">
